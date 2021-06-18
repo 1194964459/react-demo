51acb4c3f3c4187c8fa4e2d20caed96f21e43e4c
yanglixia <yanglixia568@163.com>
2021-06-18 16:21:26 +0800

    react 状态提升

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// 自我测试
+// 状态提升
+import Calculator from './test/Calculator'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Calculator />
   </React.StrictMode>,
   document.getElementById('root')
 );
