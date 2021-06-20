3f232a39ed56130bed99d6e09c7c8d2432734892
yanglixia <yanglixia568@163.com>
2021-06-20 19:57:16 +0800

    setState 补充

--- src/test/setState_parent.jsx
+++ src/test/setState_parent.jsx
@@ -15,38 +15,38 @@ class Counter extends React.Component{
 
     
         // 第一种情况：参数为对象，更新会合并，只更新一次，即界面展示为1
-        // this.setState({number: this.state.number + 1}, ()=>{
-        //     console.log('回调 1：', this.state.number)  // 1
-        // })
-        // console.log('第 1 次更新：', this.state.number)  // 0
+        this.setState({number: this.state.number + 1}, ()=>{
+            console.log('回调 1：', this.state.number)  // 1
+        })
+        console.log('第 1 次更新：', this.state.number)  // 0
 
-        // this.setState({number: this.state.number + 1}, ()=>{
-        //     console.log('回调 2：', this.state.number)  // 1
-        // })
-        // console.log('第 2 次更新：', this.state.number) // 0
+        this.setState({number: this.state.number + 1}, ()=>{
+            console.log('回调 2：', this.state.number)  // 1
+        })
+        console.log('第 2 次更新：', this.state.number) // 0
 
         // 第二种情况：参数为函数，更新不会合并，会更新两次，即界面展示为2
-        // this.setState(state => ({number:state.number + 1}), ()=>{
-        //     console.log('回调 1：', this.state.number)  // 2
-        // })
-        // console.log('第 1 次更新：', this.state.number)  // 0
+        this.setState(state => ({number:state.number + 1}), ()=>{
+            console.log('回调 1：', this.state.number)  // 2
+        })
+        console.log('第 1 次更新：', this.state.number)  // 0
 
-        // this.setState(state => ({number:state.number + 1}), ()=>{
-        //     console.log('回调 2：', this.state.number)  // 2
-        // })
-        // console.log('第 2 次更新：', this.state.number) // 0
+        this.setState(state => ({number:state.number + 1}), ()=>{
+            console.log('回调 2：', this.state.number)  // 2
+        })
+        console.log('第 2 次更新：', this.state.number) // 0
 
         // 第三种情况：第一次更新，参数是对象；第二次更新，参数是函数！
-        // this.setState({number: this.state.number + 1}, ()=>{
-        //     console.log('回调 1：', this.state.number)  // 2
-        // })
-        // console.log('第 1 次更新：', this.state.number)  // 0
-        // this.setState(state => ({number:state.number + 1}), ()=>{
-        //     console.log('回调 2：', this.state.number)  // 2
-        // })
-        // console.log('第 2 次更新：', this.state.number) // 0
+        this.setState({number: this.state.number + 1}, ()=>{
+            console.log('回调 1：', this.state.number)  // 2
+        })
+        console.log('第 1 次更新：', this.state.number)  // 0
+        this.setState(state => ({number:state.number + 1}), ()=>{
+            console.log('回调 2：', this.state.number)  // 2
+        })
+        console.log('第 2 次更新：', this.state.number) // 0
 
-        // 第四种情况：
+        // 第四种情况：同步更新
         setTimeout(()=>{
             this.setState({number: 1})
             console.log(this.state.number)  // 1
